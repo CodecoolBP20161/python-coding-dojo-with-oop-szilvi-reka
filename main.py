5f9156965a3caf4a7db073a3acab05dd689cf9a2
rekan <rekanagy06@gmail.com>
2016-09-20 09:47:17 +0200

    implement Contact class and Supplier(partially)

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
 
 class Contact:
-    pass
+
+    all_contacts = []
+
+    def __init__(self, name, email):
+        self.name = name
+        self.email = email
+        self.all_contacts.append(self)
+
+    @classmethod
+    def reset_contacts(cls):
+        del cls.all_contacts[:]
+        return cls.all_contacts
+
+
+class Supplier(Contact):
+    all_orders = {}
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def order(self, str):
